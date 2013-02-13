680be988a4d42ffc931a4bb52ee74e99741e1686
Gian Giovani <wejick@gmail.com>
2013-02-13 11:40:49 +0700

    ADD : Centron MVC to view host list

--- CodeIgniter_2.1.3/application/models/centreon_model.php
+++ CodeIgniter_2.1.3/application/models/centreon_model.php
@@ -1,9 +1,22 @@
 <?php
-class Centreon_Model extends CI_model
+class Centreon_model extends CI_model
 {
 	function __construct()
 	{
 		parents::__construct();
+		$this->load->database();
 	}
-	functio
+	function get_host_list()
+	{
+		$query = $this->db->query("SELECT
+   									 `host`.`host_id`
+    								 , `host`.`host_name`
+    								 , `host`.`host_address`
+    								 , `command`.`command_name`
+								 FROM
+    								 `centreon2`.`host`
+    								 INNER JOIN `centreon2`.`command` 
+    								     ON (`host`.`command_command_id` = `command`.`command_id`);");
+		return $query->result_array();
+	}
 }
