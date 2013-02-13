ee1e940a95cee2a3ef677914e4dfa1c812647ce5
Gian Giovani <wejick@gmail.com>
2013-02-13 13:02:12 +0700

    FIX : little homework to make sure app looks good

--- CodeIgniter_2.1.3/application/controllers/cen_cont.php
+++ CodeIgniter_2.1.3/application/controllers/cen_cont.php
@@ -4,8 +4,18 @@ class Cen_cont extends CI_Controller {
 
 	public function index()
 	{
+		$data['title'] = "Sistem Pembuatan laporan Centreon";
+		$this->load->view('monitor/header', $data);
+		$this->load->view('monitor/monitor_index');
+		$this->load->view('monitor/footer');
+	}
+	public function view_host()
+	{
+		$data['title'] = "Sistem Pembuatan laporan Centreon : Daftar Host";
 		$this->load->model('centreon_model');
-		$data[host] = $this->centreon_model->get_host_list();
-		$this->load->view('monitor/monitor_index', $data);
+		$data['host'] = $this->centreon_model->get_host_list();
+		$this->load->view('monitor/header', $data);
+		$this->load->view('monitor/monitor_hostlist', $data);
+		$this->load->view('monitor/footer');
 	}
 }
