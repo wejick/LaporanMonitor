3df9e11b21909fc8275f0158b407332086eceeaf
Gian Giovani <wejick@gmail.com>
2013-02-21 10:47:52 +0700

    ADD : add comment
    hope i can write comment for better documentation

--- CodeIgniter_2.1.3/application/models/centreon_storage_model.php
+++ CodeIgniter_2.1.3/application/models/centreon_storage_model.php
@@ -4,10 +4,21 @@ class Centreon_storage_model extends CI_Model {
 
 	function __construct()
 	{
+		// multiple database handle mechanism
 		$this->db_cent_store = $this->load->database('centreon_storage', TRUE);
 	}
+	// get list of host log
 	function get_host_log()
 	{
+		/*
+		UP_A is How much host get up
+		UP_T is Total uptime
+		DOWN_A is How much host to get down
+		DONW_T is Total downtime
+		UNREACHABLE_A is How much host get unreachable
+		UNREACHABLE_T is Total unreachable time
+		UNDETERMINED_T is Total time for uncategorized condition
+		*/
 		$query = $this->db_cent_store->query("SELECT
 			host_id,
 			sum(`UPnbEvent`) as UP_A,
@@ -21,22 +32,20 @@ class Centreon_storage_model extends CI_Model {
 			  GROUP BY `host_id`");
 		return $query->result_array();
 	}
-	function get_month_list()
+	/*
+	get list of monitored year, we will display full 12 month + years from this fuction
+	at view
+	*/
+	function get_year_list()
 	{
 		$query = $this->db_cent_store->query("SELECT
-			min('date_start') as start_date,
-			max('date_end') as end_date
+			min(`date_start`) as start_date,
+			max(`date_end`) as end_date
 			from `log_archive_host` ");
 		$result = $query->result_array();
-		$start_date = getdate($result['start_date']);
-		$end_date = getdate($result['end_date']);
-		$date_constraint = array(start_month, start_year, end_month, end_year);
-		$date_constraint['start_month'] = $start_date['mon'];
-		$date_constraint['start_year'] = $start_date['year'];
-		$date_constraint['end_month'] = $end_date['mon'];
-		$date_constraint['end_year'] = $start_date['year'];
-
-		return $date_constraint;
+		// get year array
+		$years = range(date('Y',$result[0]['start_date']), date('Y',$result[0]['end_date']));
+		return $years;
 	}
 
 }
