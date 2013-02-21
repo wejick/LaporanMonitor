a2a1b891cb9483b8173b915fd7cd0fec8511ba5c
Gian Giovani <wejick@gmail.com>
2013-02-21 15:05:13 +0700

    ADD : This is for comment for previous changes
    Previous changes is :
    ADD filter by date at view_host

--- CodeIgniter_2.1.3/application/views/monitor/monitor_index.php
+++ CodeIgniter_2.1.3/application/views/monitor/monitor_index.php
@@ -6,9 +6,9 @@
 		<p>Apa yang anda butuhkan saat ini ?.</p>
 
 		<ol>
-			<li><a href=" <?php echo site_url("cen_cont/view_host")?>" >Daftar Host</a></li>
-			<li><a href=" <?php echo site_url("cen_cont/view_log")?>">Laporan Host Bulan ini</a></li>
-			<li><a href="#">Detail laporan host</a></li>
+			<li><a href=" <?php echo site_url("cen_cont/view_host")?>" >Daftar Host.</a></li>
+			<li><a href=" <?php echo site_url("cen_cont/view_log")?>">Laporan Host Bulan ini.</a></li>
+			<li><a href="#">Detail laporan host.</a></li>
 		</ol>
 	</div>
 </div>
